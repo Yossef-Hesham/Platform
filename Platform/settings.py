b62a96cbdbc33aacf02c8c569b650263b4a19d18
youssef <youss4923@gmail.com>
2025-08-21 14:40:09 +0300

    cors

--- Platform/settings.py
+++ Platform/settings.py
@@ -35,11 +35,11 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:5173',
-#     'http://localhost:8000',
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://localhost:8000',
 
-# ]
+]
 
 
 # Application definition
